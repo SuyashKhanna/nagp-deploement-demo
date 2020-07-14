3fff70d5ac06e618f1497787aa872b125fdc7c5c
suyashkhanna <suyash.khanna@nagarro.com>
2020-07-14 10:24:57 +0530

    initial commit

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web_Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Demo Get()
+        {
+            return new Demo(){
+                Data = "Made by Suyash Khanna"
+            };
+        }
+    }
+}
---
+++ Demo.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Web_Application
+{
+    public class Demo
+    {
+        public string Data { get; set; }
+        
+    }
+}
