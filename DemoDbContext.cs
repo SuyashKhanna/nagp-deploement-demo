8e3aa38a7745ed4ed8b3f597be7e6dbeb713588f
suyashkhanna <suyash.khanna@nagarro.com>
2020-07-14 15:00:57 +0530

    f

--- DemoDbContext.cs
+++ DemoDbContext.cs
@@ -5,6 +5,7 @@ namespace nagp_deployment_demo
 {
     public class DemoDbContext : DbContext
     {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options):base(options) {  }
         public DbSet<Demo> Demo { get; set;} 
     }
 }
