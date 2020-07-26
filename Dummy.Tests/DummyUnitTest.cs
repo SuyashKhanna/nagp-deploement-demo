41ab425371a286b93be68808e62bfe2e46044d38
suyashkhanna <suyash.khanna@nagarro.com>
2020-07-26 15:05:01 +0530

    included dummy unit testing for jenkins demo

---
+++ Dummy.Tests/DummyUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dummy.Tests
+{
+    public class DummyUnitTest
+    {
+        [Fact]
+        public void isUnitTestWorking()
+        {
+           Assert.True(true);
+        }
+    }
+}
